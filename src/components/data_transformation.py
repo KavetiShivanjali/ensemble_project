252b9408fc0e0e00dcb220ad81100cc41940bfc7
Shivanjali Kaveti <kaveti.shivanjali123@gmail.com>
2026-05-05 04:54:54 +0530

    data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,108 @@
 #categorical to numerical features
+import sys 
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path: str = os.path.join('artifacts', 'preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            logging.info("Data Transformation initiated")
+
+            # Define which columns should be ordinal-encoded and which should be scaled
+            categorical_cols = [
+                'gender', 'age', 'admission_type_id', 'discharge_disposition_id',
+                'admission_source_id', 'medical_specialty', 'diag_1', 'diag_2', 'diag_3',
+                'max_glu_serum', 'A1Cresult', 'metformin', 'repaglinide', 'nateglinide',
+                'chlorpropamide', 'glimepiride', 'acetohexamide', 'glipizide',
+                'glyburide', 'tolbutamide', 'pioglitazone', 'rosiglitazone',
+                'acarbose', 'miglitol', 'troglitazone', 'tolazamide', 'examide',
+                'citoglipton', 'insulin', 'change', 'diabetesMed'
+            ]
+            numerical_cols = ['time_in_hospital', 'num_lab_procedures', 'num_procedures',
+                              'num_medications', 'number_outpatient', 'number_emergency',
+                              'number_inpatient', 'number_diagnoses']
+            num_pipeline = Pipeline(steps=[
+                ('imputer', SimpleImputer(strategy='median')),
+                ('scaler', StandardScaler())
+            ])
+            cat_pipeline = Pipeline(steps=[
+                ('imputer', SimpleImputer(strategy='most_frequent')),
+                ('onehot', OneHotEncoder(handle_unknown='ignore')),
+                ('scaler', StandardScaler(with_mean=False))
+            ])
+            logging.info(f"Categorical columns: {categorical_cols}")
+            logging.info(f"Numerical columns: {numerical_cols}")
+            preprocessor = ColumnTransformer([
+                ('num_pipeline', num_pipeline, numerical_cols),
+                ('cat_pipeline', cat_pipeline, categorical_cols)
+            ])
+            return preprocessor
+        
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def initiate_data_transformation(self, train_path, test_path):  
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+
+            logging.info("Obtaining preprocessing object")
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = 'readmitted'
+            numerical_cols = ['time_in_hospital', 'num_lab_procedures', 'num_procedures',
+                              'num_medications', 'number_outpatient', 'number_emergency',
+                              'number_inpatient', 'number_diagnoses']
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+        
+        except Exception as e:
+            raise CustomException(e, sys)   
